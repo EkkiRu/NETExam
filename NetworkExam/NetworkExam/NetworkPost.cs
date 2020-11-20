b637e67206a921f475436eae2d2d18304cb97773
EkkiRu <63056889+EkkiRu@users.noreply.github.com>
2020-11-20 20:19:22 +0600

    Exam
    Exam

---
+++ NetworkExam/NetworkExam/NetworkPost.cs
@@ -0,0 +1,60 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text;
+
+namespace NetworkExam
+{
+    public class NetworkPost
+    {
+        public async static void GetAllPosts()
+        {
+            var getRequest = HttpWebRequest.Create($"https://jsonplaceholder.typicode.com/posts/");
+            getRequest.Method = "GET";
+            //getRequest.Headers.Add(...)
+
+            var getResponce = await getRequest.GetResponseAsync();
+            var post = new List<Post>();
+
+            using (var stream = new StreamReader(getResponce.GetResponseStream()))
+            {
+                post = JsonConvert.DeserializeObject<List<Post>>(stream.ReadToEnd());
+            }
+
+            getResponce.Close();
+        }
+
+        public async static void SetPost(Post post)
+        {
+            var getRequest = HttpWebRequest.Create($"https://jsonplaceholder.typicode.com/posts/");
+            getRequest.Method = "POST";
+            var data = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(post));
+            var getResponce = await getRequest.GetRequestStreamAsync();
+            await getResponce.WriteAsync(data, 0, data.Length);
+
+            Console.WriteLine(((HttpWebResponse)getRequest.GetResponse()).StatusDescription);
+
+            getResponce.Dispose();
+        }
+
+        public async static void GetSpecificPosts(int numberPost)
+        {
+            var getRequest = HttpWebRequest.Create($"https://jsonplaceholder.typicode.com/posts/{numberPost}");
+            getRequest.Method = "GET";
+            //getRequest.Headers.Add(...)
+
+            var getResponce =await getRequest.GetResponseAsync();
+            var post = new Post();
+
+            using (var stream = new StreamReader(getResponce.GetResponseStream()))
+            {
+                post = JsonConvert.DeserializeObject<Post>(stream.ReadToEnd());
+            }
+
+            getResponce.Close();
+
+        }
+    }
+}
---
+++ NetworkExam/NetworkExam/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using Newtonsoft.Json;
+
+namespace NetworkExam
+{
+    class Program
+    {
+
+        /*
+* Есть сайт с API. https://jsonplaceholder.typicode.com/
+
+Используя полученные навыки создайте консольное приложение способное:
+1. Создавать посты
+2. Получать список постов
+3. Получать 1 пост.
+
+3 пункта меню. Никакого другого вывода не нужно делать.
+Но необходимо полученные данные превратить в объекты. А отправляемые объекты превратить в требуемый тиа данных.
+*/
+
+
+        static void Main(string[] args)
+        {
+            var post = new Post { Body = "Привет", Id = 100, Title = "Как дела", UserId = 90 };
+            NetworkPost.SetPost(post);
+            NetworkPost.GetAllPosts();
+            NetworkPost.GetSpecificPosts(2);
+        }
+
+
+
+
+    }
+}
